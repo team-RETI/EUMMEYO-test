2ea2af62a121ebbd585ccc7ebc012d11bc0744ad
indextrown <indextrown@gmail.com>
2025-04-22 17:58:12 +0900

    Initial Commit

---
+++ EUMMEYO-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EUMMEYO-test
+//
+//  Created by 김동현 on 4/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EUMMEYO-test/EUMMEYO_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUMMEYO_testApp.swift
+//  EUMMEYO-test
+//
+//  Created by 김동현 on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct EUMMEYO_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
